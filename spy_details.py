f3f862b8341415d74995afbb98f19715b0e7f4db
mananwalia959 <mananwalia959@gmail.com>
2017-07-21 15:57:38 +0530

    my project

---
+++ spy_details.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class spy:
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.chats=[]
+
+
+class addchat:
+
+    def __init__(self,message,sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+
+spy_admin = spy('Manan', 'Mr.', 19, 4.7)
+
+
+friend_list = [spy('mridul','Mr.',20,3.8),spy('nitesh','Mr.',22,4.2)]
+addchat("hi",True)
+friend_list[0].chats.append(addchat("hi",True))
+friend_list[0].chats.append(addchat("hello",True))
+friend_list[1].chats.append(addchat("hi",True))
+friend_list[1].chats.append(addchat("hello",True))
+
+
+
